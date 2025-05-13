432c5bb0249bf3dc2aabe2407b2346c0707985cf
angelathe <angela.the@skylight.digital>
2025-05-13 13:44:39 -0400

    clean snapshot test

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -7,19 +7,22 @@ from eicr_anonymization.anonimizer import Anonymizer
 from eicr_anonymization.anonymize_eicr import anonymize_eicr_file
 
 
-@pytest.mark.parametrize('dir', list(Path('tests/test_data').iterdir()))
-def test_snapshot(dir, snapshot):
+@pytest.mark.parametrize('xml_file', list(Path('tests/test_data').rglob('*.xml')))
+def test_snapshot(xml_file, snapshot):
     """
+    Run snapshot tests
+
+    Args:
+        xml_file (str): Path to XML file to be anonymized
 
     """
-    xml_files = list(dir.glob('*.xml'))
-
     anonymizer = Anonymizer(seed=1)
+    dir = xml_file.parent
 
-    for xml_file in xml_files:
-        # Output anonmyized XML tree
-        actual = anonymize_eicr_file(xml_file, anonymizer)
-        actual_xml = etree.tostring(actual, pretty_print=True, encoding="unicode")
+    # Output anonmyized XML tree
+    actual = anonymize_eicr_file(xml_file, anonymizer)
+    actual_xml = etree.tostring(actual, pretty_print=True, encoding="unicode")
 
-        snapshot.snapshot_dir = f"tests/snapshots/{dir.name}"
-        snapshot.assert_match(actual_xml, f"{xml_file.stem}_anonymized_snapshot.xml")
+    # Snapshot return value
+    snapshot.snapshot_dir = f"tests/snapshots/{dir.name}"
+    snapshot.assert_match(actual_xml, f"{xml_file.stem}_anonymized_snapshot.xml")
