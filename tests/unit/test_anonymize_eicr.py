24c1610cb0044d3114c5bdb02c97ceefdc7019f4
Josh Nygaard <jnygaard@skylight.digital>
2025-06-16 14:56:51 -0400

    fix the name and add documentation for anonymize_eicr test

--- tests/unit/test_anonymize_eicr.py
+++ tests/unit/test_anonymize_eicr.py
@@ -1,8 +1,10 @@
+"""Unit tests for the eicr_anonymization module."""
 import pytest
 from lxml import etree
 
 from eicr_anonymization.anonymize_eicr import anonymize_eicr_file
 from eicr_anonymization.anonymizer import Anonymizer
+from eicr_anonymization.element_parser import Parser
 
 
 def test_anonymize_eicr_file_empty_file():
@@ -10,7 +12,8 @@ def test_anonymize_eicr_file_empty_file():
 
     If the file is empty it should raise an etree.XMLSyntaxError.
     """
-    anonymizer = Anonymizer(seed=1)
+    anonymizer = Anonymizer()
+    parser = Parser()
     xml_file = "tests/unit/test_data/empty.xml"
     with pytest.raises(etree.XMLSyntaxError):
-        anonymize_eicr_file(xml_file, anonymizer, debug=True)
+        anonymize_eicr_file(xml_file, anonymizer, parser)
